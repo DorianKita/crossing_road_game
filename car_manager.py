714e80072139460c31e3167df1b54df0b0a8d752
Dorian Kita <dorian.kita90@gmail.com>
2024-12-05 20:35:14 +0100

    created generating cars in random places

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,30 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-STARTING_MOVE_DISTANCE = 5
+STARTING_MOVE_DISTANCE = 15
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.position_y = random.randint(-250,250)
+        self.position_x = random.randint(300, 1000)
+        self.penup()
+        self.shape('square')
+        self.shapesize(stretch_wid=1, stretch_len=1.7)
+        self.color(random.choice(COLORS))
+        self.goto(self.position_x, self.position_y)
+
+
+    def move(self):
+        new_x = self.xcor() - STARTING_MOVE_DISTANCE
+        self.goto(new_x, self.position_y)
+        if new_x <-300:
+            new_x = random.randint(300, 1000)
+            new_y = random.randint(-250,250)
+            self.goto(new_x, new_y)
+
+    def generate_new_starting_point(self):
+        self.goto(x=self.position_x, y=self.position_y)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 from turtle import Screen
 from player import Player
@@ -5,18 +6,31 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
-player = Player()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+
+cars = []
+
+for _ in range(20):
+    new_car = CarManager()
+    cars.append(new_car)
 
 screen.listen()
 screen.onkey(player.move, "w")
 
-
 game_is_on = True
 while game_is_on:
+
     time.sleep(0.1)
     screen.update()
 
+    for car in cars:
+        car.move()
+
+
     player.reset_position()
+
 
